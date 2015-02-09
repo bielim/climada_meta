154b3e2ec6a566d0b188329e9453848423a7da2b
Melanie Bieli <melanie.bieli@bluewin.ch>
2015-02-09 23:43:36 +0100

    Initial commit for climada_meta

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# climada_meta
-Meta repository for climada
+Climada meta repository
+=======================
+
+This repository is supposed to make downloading and contributing to Climada easier.
+
+To download all of Climada in one step:
+
+- clone this repository
+- if you operate on a fork, copy climada_meta.conf.default to climada_meta.conf and edit climada_meta.conf to match your setup (file intended to be self documenting)
+- run update_climada.sh
+
+
+Requirements
+------------
+
+A POSIX compliant shell (generally available on UNIX systems, on Windows Cygwin or GitBash is sufficient)
---
+++ climada_meta_update.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+# Set defaults
+CLIMADA_BASE="https://github.com/davidnbresch"
+
+# Semi hardcoded
+CLIMADA_META_CONFIG="./climada_meta.conf"
+CLIMADA_REPOSITORIES="climada
+		      climada_module_kml_toolbox 
+		      climada_module_GDP_entity
+		      climada_module_meteorites 
+		      climada_module_barisal_demo 
+		      climada_module_tc_hazard_advanced 
+		      climada_module_country_risk 
+		      climada_module_tc_rain 
+		      climada_module_eq_global 
+		      climada_module_tc_surge 
+		      climada_module_etopo 
+		      climada_module_ws_europe"
+
+########################
+## Code below here
+
+initialize_repository() {
+
+	REPOSITORY=${1}
+
+	echo
+	echo " ------ initializing \033[35m${REPOSITORY}\033[0m ------- "
+	echo
+
+	STATUS=0
+	OLD_PWD=${PWD}
+
+	if [ ${FORK} = true ]; then
+		git clone ${CLIMADA_BASE}/${REPOSITORY} ${REPOSITORY} \
+							2>&1 >/dev/null && \
+		cd ${REPOSITORY} && \
+		git remote add upstream ${CLIMADA_DEFAULT_BASE}/${REPOSITORY} \
+							2>&1 >/dev/null && \
+		git fetch upstream 2>&1 >/dev/null
+		STATUS=$?
+	else
+		git clone ${CLIMADA_BASE}/${REPOSITORY} ${REPOSITORY}
+		STATUS=$?
+	fi
+
+	cd ${OLD_PWD}
+
+	echo
+
+	if [ ${STATUS} -eq 0 ]; then 
+		echo " ------           \033[32mSUCCESS\033[0m      ------"
+	else
+		echo " ------           \033[31mFAILURE\033[0m      ------"
+	fi
+
+	echo
+
+}
+
+update_repository() {
+
+	REPOSITORY=${1}
+
+	echo
+	echo " ------ updating \033[35m${REPOSITORY}\033[0m ------- "
+	echo
+
+	STATUS=0
+	OLD_PWD=${PWD}
+
+	if [ ${FORK} = true ]; then
+		cd ${REPOSITORY} && \
+		git fetch upstream 2>&1 >/dev/null && \
+		git checkout master 2>&1 >/dev/null && \
+		git rebase upstream/master
+		STATUS=$?
+	else
+		cd ${REPOSITORY} && \
+		git pull
+		STATUS=$?
+	fi
+
+	cd ${OLD_PWD}
+
+	echo
+
+	if [ ${STATUS} -eq 0 ]; then 
+		echo " ------           \033[32mSUCCESS\033[0m      ------"
+	else
+		echo " ------           \033[31mFAILURE\033[0m      ------"
+	fi
+
+	echo
+
+}
+
+# We'll later detect if the defaults have been overridden
+CLIMADA_DEFAULT_BASE="${CLIMADA_BASE}"
+
+# Load configuration
+if [ -r ${CLIMADA_META_CONFIG} ]; then
+	. ${CLIMADA_META_CONFIG}
+fi
+
+# Determine whether we deal with a fork or not
+FORK=false
+if [ "${CLIMADA_BASE}" != "${CLIMADA_DEFAULT_BASE}" ]; then
+	FORK=true
+fi
+
+for climada_repository in $CLIMADA_REPOSITORIES; do
+
+	if [ ! -d $climada_repository ]; then
+		initialize_repository $climada_repository
+	else
+		update_repository $climada_repository
+	fi
+
+done
